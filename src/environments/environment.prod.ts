04a1d9278e1de7927de66d253c91e8ae3002c5de
Rico <272355332@qq.com>
2019-04-29 19:08:12 +0800

    清理无用文件

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  accountApiBaseUrl: 'https://passport.aoscdn.com/api',
+  cloudApiBaseUrl: 'https://airmorecloudapi.aoscdn.com/api',
+  timeout: 5000,
 };
